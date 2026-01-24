63f79d04fb1f812fa11d85c3457c200c4ae5ed82
shamoo53 <aliyushamsu16@gmail.com>
2026-01-24 11:06:34 +0100

    Implement-Contract-Provider-for-Smart-Contract-Authorization
    Implement-Contract-Provider-for-Smart-Contract-Authorization

--- src/modules/sweeps/sweeps.module.ts
+++ src/modules/sweeps/sweeps.module.ts
@@ -1,19 +1,25 @@
-import { Module } from '@nestjs/common';
+import { Module } from '@nestjs/module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { SweepsService } from './sweeps.service.js';
 import { ValidationProvider } from './providers/validation.provider.js';
 import { ContractProvider } from './providers/contract.provider.js';
-import { TransactionProvider } from './providers/transaction.provider.js';
 import { Account } from '../accounts/entities/account.entity.js';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Account])],
-  providers: [
-    SweepsService,
-    ValidationProvider,
-    ContractProvider,
-    TransactionProvider,
-  ],
+  providers: [SweepsService, ValidationProvider, ContractProvider],
   exports: [SweepsService],
 })
 export class SweepsModule {}
+Update src/config/stellar.config.ts
+import { registerAs } from '@nestjs/config';
+
+export default registerAs('stellar', () => ({
+  network: process.env.STELLAR_NETWORK ?? 'testnet',
+  horizonUrl: process.env.STELLAR_HORIZON_URL ?? 'https://horizon-testnet.stellar.org',
+  sorobanRpcUrl: process.env.STELLAR_SOROBAN_RPC_URL ?? 'https://soroban-testnet.stellar.org',
+  fundingSecret: process.env.STELLAR_FUNDING_SECRET,
+  contracts: {
+    ephemeralAccount: process.env.EPHEMERAL_ACCOUNT_CONTRACT_ID,
+  },
+}));
