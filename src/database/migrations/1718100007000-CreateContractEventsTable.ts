65b3628cb4d169cfebd36758061d47cec36b2735
AJ0070 <jashambaliya1@gmail.com>
2026-06-26 10:18:37 +0530

    Format contract events migration

--- src/database/migrations/1718100007000-CreateContractEventsTable.ts
+++ src/database/migrations/1718100007000-CreateContractEventsTable.ts
@@ -1,8 +1,6 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
 
-export class CreateContractEventsTable1718100007000
-  implements MigrationInterface
-{
+export class CreateContractEventsTable1718100007000 implements MigrationInterface {
   name = 'CreateContractEventsTable1718100007000';
 
   public async up(queryRunner: QueryRunner): Promise<void> {
